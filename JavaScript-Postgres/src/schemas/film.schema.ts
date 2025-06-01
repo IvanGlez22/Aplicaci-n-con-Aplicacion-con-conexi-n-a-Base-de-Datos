a4052dc1b31867f9b084ca4ceba74a777d7ae1a2
Francisco Iván González González <158486338+IvanGlez22@users.noreply.github.com>
2025-05-31 22:48:40 -0600

    Delete JavaScript-Postgres directory

--- JavaScript-Postgres/src/schemas/film.schema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const FilmSchema = z.object({
-    title: z.string().min(10, "El titulo es obligatorio con la longitud minima de 10"),
-    description: z.string().min(10, "La descripicion es obligatorio"),
-});
--- JavaScript-Postgres/src/services/actor.service.ts
+++
@@ -1,8 +0,0 @@
-import { ActorRepository } from '../repositories/actor.repos.ts';
-
-export const ActorService = {
-    getAll: () => ActorRepository.findAll(),
-    getById: (id: number) => ActorRepository.findById(id),
-    add: (first_name: string, last_name: string) =>
-        ActorRepository.add({ first_name, last_name }),
-};
